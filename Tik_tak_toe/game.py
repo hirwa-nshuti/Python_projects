f2d88af4a34be4918b2a3c98310247781a5a61e1
hirwa-nshuti <hirwanshutiflx@gmail.com>
2021-07-28 23:10:27 +0200

    Displaying modes added

--- Tik_tak_toe/game.py
+++ Tik_tak_toe/game.py
@@ -40,3 +40,41 @@ y_img = pg.image.load('o_modified.png')
 initiating_window = pg.transform.scale(initiating_window, (width, height + 1))
 x_img = pg.transform.scale(x_img, (80, 80))
 y_img = pg.transform.scale(y_img, (80, 80))
+
+
+def game_initiating_window():
+
+    screen.blit(initiating_window, (0, 0))
+
+    # Updating display
+    pg.display.update()
+    time.sleep(3)
+    screen.fill(white)
+
+    # Vertical lines
+    pg.draw.line(screen, line_color, (width / 3, 0), (width / 3, height), 7)
+    pg.draw.line(screen, line_color, (width / 3 * 2, 0), (width / 3 * 2, height), 7)
+
+    # horizontal lines
+    pg.draw.line(screen, line_color, (0, height / 3), (width, height / 3), 7)
+    pg.draw.line(screen, line_color, (0, height / 3 * 2), (width, height / 3 * 2), 7)
+    draw_status()
+
+
+def draw_status():
+    global draw
+
+    if winner is None:
+        message = XO.upper() + "'s Turn"
+    else:
+        message = winner.upper() + "won !"
+    if draw:
+        message = "Game Draw !"
+
+    font = pg.font.Font(None, 30)
+    text = font.render(message, True, (255, 255, 255))
+
+    screen.fill((0, 0, 0), (0, 400, 500, 100))
+    text_rect = text.get_rect(center=(width / 2, 500-50))
+    screen.blit(text, text_rect)
+    pg.display.update()
