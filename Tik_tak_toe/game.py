0d131dab13eaf9f0c7afec26c154c329a867093a
hirwa-nshuti <hirwanshutiflx@gmail.com>
2021-07-31 17:29:02 +0200

    added images

--- Tik_tak_toe/game.py
+++ Tik_tak_toe/game.py
@@ -171,3 +171,27 @@ def user_click():
         drawXO(row, col)
         check_win()
 
+
+def reset_game():
+    global board, winner, XO, draw
+    time.sleep(3)
+    XO = 'x'
+    draw = False
+    game_initiating_window()
+    winner = None
+    board = [[None]*3, [None]*3, [None]*3]
+
+
+game_initiating_window()
+
+while True:
+    for event in pg.event.get():
+        if event.type == QUIT:
+            pg.quit()
+            sys.exit()
+        elif event.type is MOUSEBUTTONDOWN:
+            user_click()
+            if winner or draw:
+                reset_game()
+    pg.display.update()
+    CLOCK.tick(fps)
